822ecb1164ed5baed380d414fe610df578338b09
shouhei <shohei.nagamura@gmail.com>
2023-01-31 18:25:36 +0900

    Initial Commit

---
+++ omikuji/omikujiApp.swift
@@ -0,0 +1,17 @@
+//
+//  omikujiApp.swift
+//  omikuji
+//
+//  Created by shouhei nagamura on 2023/01/31.
+//
+
+import SwiftUI
+
+@main
+struct omikujiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
