b7604d5261b3e3c124662b7cfbe20f6a1a7b6346
shouhei <shohei.nagamura@gmail.com>
2023-01-31 19:54:05 +0900

    first commit

--- omikuji/ContentView.swift
+++ omikuji/ContentView.swift
@@ -8,15 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var imageName = "omikuji"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing:32){
+            Image(imageName)
+                .resizable()
+                .frame(maxWidth: .infinity)
+            HStack(spacing:16){
+                Button("リセット"){
+                    imageName = "omikuji"
+                }
+                .frame(maxWidth: .infinity, minHeight: 50) //サイズ
+                .background(.red)  //背景のViewを設定するModifier, ColorというViewのgreenを指定(型推論でColorを省略)
+                .foregroundColor(.white) //前面に表示されるView(ここでは"おみくじを引く"というText)の色を設定できるModifier
+                
+                Button("おみくじを引く"){
+                    updateOmikujiImageView()
+                }
+                .frame(maxWidth: .infinity, minHeight: 50) //サイズ
+                .background(.green)  //背景のViewを設定するModifier, ColorというViewのgreenを指定(型推論でColorを省略)
+                .foregroundColor(.white) //前面に表示されるView(ここでは"おみくじを引く"というText)の色を設定できるModifier
+            }
+            
         }
-        .padding()
+        .padding(40.0)
     }
+    
+    func updateOmikujiImageView() { // 関数名はたいてい動詞が最初にきます
+       let luckNumber = Int.random(in: 0...2)
+       if luckNumber == 0 {
+         imageName = "daikichi"
+       } else if luckNumber == 1 {
+         imageName = "kichi"
+       } else {
+         imageName = "kyou"
+       }
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
